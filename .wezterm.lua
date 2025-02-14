3752fae68ab318e940738af8d5b1c52286605aa4
denan63 <denan63@gmail.com>
2025-02-14 18:15:38 +0100

    Terminal blue color change
    Change the hex code of blue color and set equal for light blue

--- .wezterm.lua
+++ .wezterm.lua
@@ -15,8 +15,8 @@ config.colors = {
 	cursor_fg = "#011423",
 	selection_bg = "#033259",
 	selection_fg = "#CBE0F0",
-	ansi = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#0FC5ED", "#a277ff", "#24EAF7", "#24EAF7" },
-	brights = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#A277FF", "#a277ff", "#24EAF7", "#24EAF7" },
+	ansi = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#0FA6ED", "#A277FF", "#24EAF7", "#24EAF7" },
+	brights = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#0FA6ED", "#A277FF", "#24EAF7", "#24EAF7" },
 }
 
 config.font = wezterm.font("MesloLGS Nerd Font Mono")
